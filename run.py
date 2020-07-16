e0ad52b81b6d2cec680b5fce60209ec02141110c
Roman Shishkin <spark@uwtech.org>
2020-07-16 18:53:59 +0300

    Move to Python 3.8 and update Dockerfile

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import os
 from andrew import AndrewBot
 
-config_file = os.environ.get('ANDREW_BOT_CONFIG', 'local_config.LocalConfig')
-print('Start using {}'.format(config_file))
-
 andrew_bot = AndrewBot()
-andrew_bot.config.apply(config_file)
+andrew_bot.config.apply('andrew.config.BaseConfig')
 andrew_bot.run()
